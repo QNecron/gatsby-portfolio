70c378296676331f9544b2e183fe027026307077
Unknown <q.necron@gmail.com>
2021-04-15 23:18:42 -0500

    404 needs data too, as do any and all pages in the future

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,24 @@
 import * as React from "react"
+// import { Link } from "gatsby"
+
+import Data from "../../json/data.json"
 
 import Seo from "../components/seo/seo"
 import Page from "../components/page/page"
+import Section from "../components/section/section"
 
 const NotFoundPage = () => (
-  <Page>
+
+  <Page data={Data}>
+
     <Seo title="404: Not found" />
-    <h1>404</h1>
-    <p>Error, error, intruder alert!</p>
+
+    <Section id="404">
+      <h1>404</h1>
+      <p>Error, error, intruder alert!</p>
+    </Section>
+
+
   </Page>
 )
 
